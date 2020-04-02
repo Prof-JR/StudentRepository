977a719d449fff45e762cfbfcace4d7e65623c02
Prof-JR <38041799+Prof-JR@users.noreply.github.com>
2020-04-02 15:39:44 -0400

    Add files via upload

---
+++ StudentRepository_Jim_Rowland.py
@@ -0,0 +1,13 @@
+""" 
+    This file has the code for the initial release of the student repository with 
+    the Student and Instructor PrettyTables
+"""
+
+class Student:
+    pass
+
+class Instructor:
+    pass
+
+class Repository:
+    pass
